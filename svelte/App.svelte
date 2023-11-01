3a07a01a4d1667cde00cb1a98e2ef2573df51462
jenyor <yurii.shymon@knu.ua>
2023-11-01 15:57:28 +0200

    lab 5

---
+++ svelte/App.svelte
@@ -0,0 +1,46 @@
+<script>
+	import { createTodoStore } from './todos.js';
+	import TodoList from './TodoList.svelte';
+
+	const todos = createTodoStore();
+</script>
+
+<div class="board">
+	<input
+		placeholder="Todo"
+		on:keydown={(e) => {
+			if (e.key === 'Enter') {
+				todos.add(e.currentTarget.value);
+				e.currentTarget.value = '';
+			}
+		}}
+	/>
+
+	<div class="todo">
+		<h2>todo</h2>
+		<TodoList store={todos} done={false} />
+	</div>
+
+	<div class="done">
+		<h2>done</h2>
+		<TodoList store={todos} done={true} />
+	</div>
+</div>
+
+<style>
+	.board {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-column-gap: 1em;
+	}
+
+	.board > input {
+		font-size: 1.4em;
+		grid-column: 1/3;
+	}
+
+	h2 {
+		font-size: 2em;
+		font-weight: 200;
+	}
+</style>
---
+++ svelte/markdown-preview.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { marked } from 'marked';
+	let value = "";
+</script>
+
+<div class="grid">
+	input
+	<textarea bind:value></textarea>
+
+	output
+	<div>{@html marked(value)}</div>
+</div>
+
+<style>
+	.grid {
+		display: grid;
+		grid-gap: 1em;
+	}
+
+	textarea {
+		flex: 1;
+		resize: none;
+		min-height: 200px;
+	}
+</style>
